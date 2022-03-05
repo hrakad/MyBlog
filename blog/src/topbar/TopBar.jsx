d074e833036af0787f27862eaa7bc676ddb01b84
hrakad <h.alrakad82@hotmail.com>
2022-03-05 16:33:45 -0700

    Add some icons for the toplift and some text for the topCenter

--- blog/src/topbar/TopBar.jsx
+++ blog/src/topbar/TopBar.jsx
@@ -3,7 +3,22 @@ import "./topbar.css"
 export default function TopBar() {
   return (
     <div className="topbar">
-      topbar
+      <div className="topLeft">
+      <i class="fa-brands fa-facebook"></i>
+      <i class="fa-brands fa-twitter"></i>
+      <i class="fa-brands fa-tiktok"></i>
+      <i class="fa-brands fa-instagram-square"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">Home</li>
+          <li className="topListItem">About</li>
+          <li className="topListItem">Contact</li>
+          <li className="topListItem">Write</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">Right</div>
     </div>
   )
 }
