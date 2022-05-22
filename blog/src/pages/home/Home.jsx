c97a1c1bec58fc3d571623d9cb77a7a71c96f99b
hrakad <h.alrakad82@hotmail.com>
2022-05-22 11:56:52 -0600

    Rearrange the files

--- blog/src/pages/home/Home.jsx
+++ blog/src/pages/home/Home.jsx
@@ -1,17 +1,16 @@
-import Header from "../../header/Header";
-import Posts from "../../posts/Posts";
-import Sidebar from "../../sidebar/Sidebar";
-import "./home.css";
-
+import Header from '../../components/header/Header';
+import Posts from '../../components/posts/Posts';
+import Sidebar from '../../components/sidebar/Sidebar';
+import './home.css';
 
 export default function Home() {
   return (
     <>
-    <Header/>
-    <div className="home">
-      <Posts/>
-      <Sidebar/>
-    </div>
+      <Header />
+      <div className="home">
+        <Posts />
+        <Sidebar />
+      </div>
     </>
-  )
+  );
 }
