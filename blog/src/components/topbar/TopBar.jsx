aa5e7c632c9f8c8c802846fc7240982c6d5c82ec
hrakad <h.alrakad82@hotmail.com>
2022-05-22 15:11:33 -0600

    Refactor the topbar list names

--- blog/src/components/topbar/TopBar.jsx
+++ blog/src/components/topbar/TopBar.jsx
@@ -1,31 +1,31 @@
-import "./topbar.css"
+import './topbar.css';
 
 export default function TopBar() {
   return (
     <div className="topbar">
       <div className="topLeft">
-      <i className="topIcons fa-brands fa-facebook"></i>
-      <i className="topIcons fa-brands fa-twitter"></i>
-      <i className="topIcons fa-brands fa-tiktok"></i>
-      <i className="topIcons fa-brands fa-instagram-square"></i>
+        <i className="topIcons fa-brands fa-facebook"></i>
+        <i className="topIcons fa-brands fa-twitter"></i>
+        <i className="topIcons fa-brands fa-tiktok"></i>
+        <i className="topIcons fa-brands fa-instagram-square"></i>
       </div>
       <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">Home</li>
-          <li className="topListItem">About</li>
-          <li className="topListItem">Contact</li>
-          <li className="topListItem">Write</li>
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
           <li className="topListItem">LOGOUT</li>
         </ul>
       </div>
       <div className="topRight">
-      <img className="topImage"
-      src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-      alt=""
-      />
-      <i className="topSearch fa-solid fa-magnifying-glass"></i>
+        <img
+          className="topImage"
+          src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+          alt=""
+        />
+        <i className="topSearch fa-solid fa-magnifying-glass"></i>
       </div>
-      
     </div>
-  )
+  );
 }
