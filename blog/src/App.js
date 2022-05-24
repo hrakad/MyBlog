6c018e946b45431af7c59628069c23e00cef5aca
hrakad <h.alrakad82@hotmail.com>
2022-05-23 23:37:52 -0600

    Add Write component to app.js

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,13 +1,16 @@
 import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Home />
       <Single />
+      <Write />
     </div>
   );
 }
