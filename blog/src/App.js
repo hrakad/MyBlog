95e601a124e6e8b52b2fb33599a0df5326957ef5
hrakad <h.alrakad82@hotmail.com>
2022-05-24 20:52:43 -0600

    Add settings componente in app.js

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <Single />
       <Write />
+      <Settings />
     </div>
   );
 }
