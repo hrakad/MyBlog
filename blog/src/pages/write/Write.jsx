08a892dcd71afd1f283a151d678f3dca995c05d9
hrakad <h.alrakad82@hotmail.com>
2022-05-24 19:47:47 -0600

    Add static image to write component

--- blog/src/pages/write/Write.jsx
+++ blog/src/pages/write/Write.jsx
@@ -3,13 +3,23 @@ import './write.css';
 export default function Write() {
   return (
     <div className="write">
+      <img
+        className="writeImg"
+        src="https://images.pexels.com/photos/6685428//pexels-photo-6685428.jpeg?auto=compress&css=tinysrgb&dpr=2&width=500"
+        alt=""
+      />
       <form className="writeForm">
         <div className="writeFormGroup">
           <label htmlFor="fileInput">
-            <i className="fa-solid fa-plus"></i>
+            <i className="writeAdd fa-solid fa-plus"></i>
           </label>
           <input type="file" id="fileInput" style={{ display: 'none' }} />
-          <input type="text" placeholder="Title" autoFocus={true} />
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+          />
         </div>
         <div className="writeFormGroup">
           <textarea
