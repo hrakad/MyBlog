31ff644e29f9404ce2ab3520e26cb17c09de2be4
hrakad <h.alrakad82@hotmail.com>
2022-05-27 13:23:08 -0600

    Add registration compponent to app.js

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,6 +4,7 @@ import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
+import Register from "./pages/Register/Register";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Write />
       <Settings />
       <Login />
+      <Register />
     </div>
   );
 }
