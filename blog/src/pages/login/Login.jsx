2dddf5df89ee8bb43569548d17115a1e8bb9999c
hrakad <h.alrakad82@hotmail.com>
2022-05-26 20:56:50 -0600

    Refactor the login page

---
+++ blog/src/pages/login/Login.jsx
@@ -0,0 +1,25 @@
+import './login.css';
+
+export default function Login() {
+  return (
+    <div className="login">
+      <span className="loginTitle">Login</span>
+      <form className="loginForm">
+        <label>Email</label>
+        <input
+          className="loginInput"
+          type="email"
+          placeholder="example@email.com"
+        ></input>
+        <label>Password</label>
+        <input
+          className="loginInput"
+          type="password"
+          placeholder="Enter your password ... "
+        ></input>
+        <button className="loginButton">Login</button>
+        <button className="registerButton">Register</button>
+      </form>
+    </div>
+  );
+}
