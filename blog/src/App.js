60e74e8ebfc9cb9d85b83f76865fa54cd7763c9c
hrakad <h.alrakad82@hotmail.com>
2022-05-30 20:30:25 -0600

    Add routes in app.js

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,22 +1,28 @@
+import { Routes, Route } from "react-router-dom"
 import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
-import Register from "./pages/Register/Register";
+import Register from "./pages/register/Register";
 
 
 function App() {
   return (
-    <div className="App">
-      <TopBar />
-      <Home />
-      <Single />
-      <Write />
-      <Settings />
-      <Login />
-      <Register />
+
+    <div className="app">
+      <header>
+        <TopBar />
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/single" element={<Single />} />
+      </Routes>
     </div>
   );
 }
