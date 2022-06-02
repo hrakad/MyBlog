ee466d2be0e42aab423ce9c24fa19ebb2747776d
hrakad <h.alrakad82@hotmail.com>
2022-06-01 22:35:27 -0600

    Create client and api folders

---
+++ blog/client/src/pages/register/Register.jsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+import './register.css';
+
+export default function Register() {
+  return (
+    <div className="register">
+      <span className="registerTitle">Register</span>
+      <form className="registerForm">
+        <label>UserName</label>
+        <input
+          className="registerInput"
+          type="userName"
+          placeholder="Enter your Name..."
+        ></input>
+        <label>Email</label>
+        <input
+          className="registerInput"
+          type="email"
+          placeholder="example@email.com"
+        ></input>
+        <label>Password</label>
+        <input
+          className="registerInput"
+          type="password"
+          placeholder="Enter your password... "
+        ></input>
+        <button className="registerLogButton">Register</button>
+        <button className="loginRegButton">
+          <Link to="/login" className="link">
+            Login
+          </Link>
+        </button>
+      </form>
+    </div>
+  );
+}
