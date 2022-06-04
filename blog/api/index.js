ce75950fcdb68e1542d953ad0f6c66de6d4a8bc4
hrakad <h.alrakad82@hotmail.com>
2022-06-04 15:45:01 -0600

    Crete connect to mongo database

--- blog/api/index.js
+++ blog/api/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose')
 
 const app = express();
 
-//dotenv.config();
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(console.log('Connected to DataBase!'))
+  .catch(err => {
+    console.log(err.message)
+  });
 
 const PORT = process.env.PORT || 3300;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/', (req, res) => {
+  console.log('Hey')
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
