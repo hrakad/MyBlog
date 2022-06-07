21ccef7dfe466724684e8dc127ebfe0697bb99ec
hrakad <h.alrakad82@hotmail.com>
2022-06-06 23:24:20 -0600

    Add route for login

--- blog/api/routers/auth.js
+++ blog/api/routers/auth.js
@@ -1,13 +1,17 @@
 const router = require('express').Router();
 const User = require('../models/User.js');
 
+const bcrypt = require('bcrypt');
+
 //Register
 router.post('/register', async (req, res) => {
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt)
     const newUser = new User({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPass,
     })
     const user = await newUser.save();
     res.status(200).json(user);
@@ -16,4 +20,23 @@ router.post('/register', async (req, res) => {
   }
 })
 
+
+//Login
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          name: user.name,
+          email: user.email,
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: 'Invalid email or password' });
+  } catch (error) {
+    res.status(500).json(err);
+  }
+})
 module.exports = router;
