65387439047d248a12227b04311855dcd7ca2ad3
hrakad <h.alrakad82@hotmail.com>
2022-06-06 21:11:10 -0600

    Refactor User schema to export it

--- blog/api/models/User.js
+++ blog/api/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = new.mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
@@ -14,4 +14,4 @@ const userSchema = new.mongoose.Schema(
 
 const User = mongoose.model('User', userSchema);
 
-export default User;
+module.exports = User;
