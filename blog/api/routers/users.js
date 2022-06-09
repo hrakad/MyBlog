1a40892128f0c19f76e505b7ae3f18ca4fc05d71
hrakad <h.alrakad82@hotmail.com>
2022-06-09 17:25:34 -0600

    Create find user route

--- blog/api/routers/users.js
+++ blog/api/routers/users.js
@@ -45,5 +45,16 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+//Get Users
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 
 module.exports = router;
