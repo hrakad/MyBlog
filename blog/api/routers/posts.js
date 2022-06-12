579ea57e449e6c95dfac05ae5ab912370916322a
hrakad <h.alrakad82@hotmail.com>
2022-06-12 11:48:26 -0600

    Create route to update post

--- blog/api/routers/posts.js
+++ blog/api/routers/posts.js
@@ -13,4 +13,29 @@ router.post('/', async (req, res) => {
   }
 });
 
+//Update Post
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
+      } catch (error) {
+        res.status(500).json(error)
+
+      }
+    } else {
+      res.status(401).send('You can not update this post!')
+    }
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 module.exports = router;
