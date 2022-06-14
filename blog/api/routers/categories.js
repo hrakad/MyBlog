288bf2fc2f32e798c1679fff496769a7fee18d24
hrakad <h.alrakad82@hotmail.com>
2022-06-13 23:51:05 -0600

    Create a router to fetch the entire categories

--- blog/api/routers/categories.js
+++ blog/api/routers/categories.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
 const Category = require('../models/Category');
 
+//Add Category
 router.post('/', async (req, res) => {
   const newCategory = new Category(req.body);
   try {
     const savedCategory = await newCategory.save();
     res.status(200).json(savedCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Get Categories
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
   } catch (error) {
     res.status(500).json(error);
   }
