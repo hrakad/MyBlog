89f8065d2391fc036ab1df05fcb229895dd40955
hrakad <h.alrakad82@hotmail.com>
2022-06-20 23:17:39 -0600

    Import axios to fetch data and useState

--- blog/client/src/pages/home/Home.jsx
+++ blog/client/src/pages/home/Home.jsx
@@ -2,8 +2,11 @@ import Header from '../../components/header/Header';
 import Posts from '../../components/posts/Posts';
 import Sidebar from '../../components/sidebar/Sidebar';
 import './home.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function Home() {
+  const [posts, setPosts] = useState();
   return (
     <>
       <Header />
