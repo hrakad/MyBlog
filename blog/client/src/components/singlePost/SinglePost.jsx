1650411571608c3dd2795fcc97efc771841e57e3
hrakad <h.alrakad82@hotmail.com>
2022-06-21 21:37:43 -0600

    Use props instead of hard coaded while fetching data from backend

--- blog/client/src/components/singlePost/SinglePost.jsx
+++ blog/client/src/components/singlePost/SinglePost.jsx
@@ -1,52 +1,41 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import './singlePost.css';
 
 export default function SinglePost() {
+  const location = useLocation();
+  const path = location.pathname.split('/')[2];
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    const getPost = async () => {
+      const resp = await axios.get('http://localhost:3300/api/posts/' + path);
+      setPost(resp.data);
+    };
+    getPost();
+  }, [path]);
+
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
-        <img
-          className="singlePostImg"
-          alt=""
-          src="https://images.pexels.com/photos/6685428//pexels-photo-6685428.jpeg?auto=compress&css=tinysrgb&dpr=2&width=500"
-        />
+        {post.photo && (
+          <img className="singlePostImg" alt="" src={post.photo} />
+        )}
         <h1 className="singlePostTitle">
-          {' '}
-          Lorem ipsum dolor sit amet.
+          {post.title}
           <i className="singlePostIcon fa-solid fa-trash-can"></i>
           <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
-            Author: <b>John Doe</b>
+            Author: <b>{post.username}</b>
           </span>
-          <span className="singlePostDate">1hour ago</span>
+          <span className="singlePostDate">
+            {new Date(post.createdAt).toDateString()}
+          </span>
         </div>
-        <p className="singlePostDesc">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus in
-          consectetur tenetur, distinctio ex, dolorum harum porro assumenda
-          minima ea nostrum, eaque dolores! Error autem nobis corporis harum
-          pariatur alias eum excepturi voluptatibus deserunt facere incidunt,
-          ducimus deleniti asperiores fugiat quae quidem non impedit possimus,
-          repellat ipsam dolorum omnis molestias id. Earum delectus facilis
-          necessitatibus? Aut perspiciatis odit praesentium minima odio
-          accusamus, consectetur illum fuga, soluta beatae repudiandae a
-          numquam. Sit laborum illum, perspiciatis dolorum deserunt itaque,
-          tenetur animi aspernatur voluptatum saepe dignissimos quo. Corrupti,
-          voluptas id! Sunt, voluptate, aliquid corrupti aliquam voluptates
-          dicta recusandae perferendis ipsum hic laborum, iste quo non est
-          similique vel adipisci voluptas quia dolores voluptatem? Ratione,
-          cumque deserunt! Fugit facilis modi eos ullam, omnis incidunt
-          molestiae dolorum dignissimos similique eligendi itaque provident
-          quam, velit impedit voluptatem odit ducimus id esse ex! Accusantium
-          fugit dolorem, nobis nesciunt sapiente odit, corporis error sunt
-          ratione, debitis iure? Quas voluptatibus repellat ipsa accusantium
-          minima laudantium aut sunt. Reprehenderit aspernatur doloremque
-          repudiandae quae? Veniam, expedita aut sed non iure id laudantium
-          officia tempore magnam molestias, doloremque beatae repellat eum quos
-          tenetur iste aliquam. Voluptatum consectetur numquam laudantium
-          aliquid nostrum tempore ullam voluptatem, sit maxime voluptas, aperiam
-          porro molestias ut iste.
-        </p>
+        <p className="singlePostDesc">{post.desc}</p>
       </div>
     </div>
   );
