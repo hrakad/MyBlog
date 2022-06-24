c40e204b9be462119e74856a70e420583f28e4f9
hrakad <h.alrakad82@hotmail.com>
2022-06-23 22:01:13 -0600

    Add useLocation hook to return the location object that represents the current URL to return a new location whenever the URL changes

--- blog/client/src/pages/home/Home.jsx
+++ blog/client/src/pages/home/Home.jsx
@@ -4,18 +4,20 @@ import Sidebar from '../../components/sidebar/Sidebar';
 import './home.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const resp = await axios.get('http://localhost:3300/api/posts');
+      const resp = await axios.get('http://localhost:3300/api/posts' + search);
       //console.log(resp);
       setPosts(resp.data);
     };
     fetchPosts();
-  }, []);
+  }, [search]);
 
   return (
     <>
