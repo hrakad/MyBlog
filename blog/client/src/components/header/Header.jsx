16e78e3194cb4bb53859c837aefea7e9626c3176
hrakad <h.alrakad82@hotmail.com>
2022-06-26 23:16:05 -0600

    Use a new URL for header image

--- blog/client/src/components/header/Header.jsx
+++ blog/client/src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import "./header.css"
+import './header.css';
 
 export default function Header() {
   return (
@@ -7,10 +7,11 @@ export default function Header() {
         <span className="headerTitleSm">R & N</span>
         <span className="headerTitleLg">Blog</span>
       </div>
-      <img className="headerImg"
-      src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-      alt=""
+      <img
+        className="headerImg"
+        src="https://images.pexels.com/photos/5833890/pexels-photo-5833890.jpeg?cs=srgb&dl=pexels-james-wheeler-5833890.jpg&fm=jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
       />
     </div>
-  )
+  );
 }
